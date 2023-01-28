f0a331f4111566020198457cdf6e27a2d1ee95e0
imalhaas <lucasdecassia9@hotmail.com>
2023-01-28 10:23:54 -0300

    teste

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO tb_todolist (titulo, done) values ('Super Mercado', 'true');
-INSERT INTO tb_todolist (titulo, done) values ('Farmacia', 'true');
-INSERT INTO tb_todolist (titulo, done) values ('Padaria', 'true');
-INSERT INTO tb_todolist (titulo, done) values ('Faculdade', 'true');
-INSERT INTO tb_todolist (titulo, done) values ('Academia', 'true');
-INSERT INTO tb_todolist (titulo, done) values ('Hospital', 'false');
-INSERT INTO tb_todolist (titulo, done) values ('Shopping', 'true');
